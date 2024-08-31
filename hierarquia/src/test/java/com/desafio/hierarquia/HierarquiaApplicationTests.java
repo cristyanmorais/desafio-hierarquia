9b0397504bafb187282dc99a288fb5a438aa43a8
Cristyan Morais <cristyan.morais123@gmail.com>
2024-08-31 15:36:46 -0300

    Criação do Projeto

---
+++ hierarquia/src/test/java/com/desafio/hierarquia/HierarquiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.hierarquia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HierarquiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
